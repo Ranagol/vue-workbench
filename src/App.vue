e8a1f7cd109d9a4d9a51a25623714ec966c4bdb4
Andor <andorhorvat@gmail.com>
2023-06-29 09:24:00 +0200

    Basic Vue app done.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="myObject.color2">0</div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+let myObject = reactive({
+  color2: 'purple'
+});
+
+
+
+</script>
+
+<style scoped>
+
+
+.purple {
+  width: 100px;
+  height: 100px;
+  background-color: purple;
+  display: inline-block;
+  margin: 10px;
+}
+
+.green {
+  background-color: green;
+  width: 100px;
+  height: 100px;
+}
+
+</style>
